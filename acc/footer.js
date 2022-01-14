33837052894573f270c24ecf7ed02afc93fe8d60
Serine-t <serinetovmasyan98@gmail.com>
2022-01-14 07:44:24 +0400

    files

---
+++ acc/footer.js
@@ -0,0 +1,9 @@
+const footer_images = [
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-newj.jpg?v=1579013167',
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-aaaqa-1.jpg?v=1579013167',
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-tamarik.jpg?v=1579013167',
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-236-alve-odonnell-651471-unsplash-1.jpg?v=1579013167',
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-newjj.jpg?v=1579013167',
+    'https://static.ucraft.net/fs/ucraft/userFiles/preview-acc/images/236-236-alve-odonnell-607084-unsplash-1.jpg?v=1579013167'
+]
+
